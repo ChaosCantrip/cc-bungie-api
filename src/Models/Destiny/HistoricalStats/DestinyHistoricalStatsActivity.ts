4c7ad518ab4031ec7acace499404fcb710b3a0cf
ChaosCantrip <dev@chaoscantrip.com>
2025-09-18 22:10:51 +0100

    Added Models.Destiny.HistoricalStats.DestinyHistoricalStatsActivity

---
+++ src/Models/Destiny/HistoricalStats/DestinyHistoricalStatsActivity.ts
@@ -0,0 +1,9 @@
+export interface DestinyHistoricalStatsActivity{
+    referenceId: number;
+    directorActivityHash: number;
+    instanceId: number;
+    mode: number;
+    modes: number[];
+    isPrivate: boolean;
+    membershipType: number;
+}
